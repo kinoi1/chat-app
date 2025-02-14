5e1accbfaea2d82ac83087a1e76208aa3e6e99d1
kinoi1 <agilsupriyanto14@gmail.com>
2025-02-14 22:53:36 +0700

    add chat list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,38 @@
+"use client"
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/bundle"; // ✅ Tambahkan ini
+import Image from "next/image";
 
+type dataChat = {
+  id: number;
+  name: string;
+  unread: number;
+};
 
 export default function Home() {
+  const personWidth = 107;
+  const personHeight = 107;
+  const personChatWidth = 50;
+  const personChatHeight = 50;
+
+  const dataChat: dataChat[] = [
+    { id: 1, name: "alex", unread: 1 },
+    { id: 2, name: "mary", unread: 4 },
+    { id: 3, name: "peter", unread: 0 },
+    { id: 4, name: "kim", unread: 4 },
+    { id: 5, name: "ben", unread: 0 },
+  ];
   return (
+    <>
     <div className="main-container">
       <div className="header-container">
-        <div className="header-text text-lg">Messages</div>
+        <div className="header-text text-2xl">Messages</div>
         <div className="header-icon">
           <span className="icon-filter"></span>
         </div>
       </div>
+      
       <div className="search-container">
         <input type="text" name="search" className="bg-search search-input text-xl" placeholder="Search...." />
         <i className="icon-search input"></i>
@@ -16,20 +40,94 @@ export default function Home() {
       <div className="active-container mt-4">
         <span className="text-base font-bold text-white inline-flex items-center">Currently Active <i className="icon-ellips"></i></span>
       </div>
+      <Swiper className="active-user-container" spaceBetween={5} slidesPerView={4} allowTouchMove={true}>
       <div className="active-user-container flex gap-2">
-        <div className="active-user bg-user-1">
-          <img src="/person/person-1.png" alt="tes" />
-        </div>
-        <div className="active-user bg-user-1">
-          <img src="/person/person-1.png" alt="tes" />
-        </div>
-        <div className="active-user bg-user-1">
-          <img src="/person/person-1.png" alt="tes" />
-        </div>
-        <div className="active-user bg-user-1">
-          <img src="/person/person-1.png" alt="tes" />
+        <SwiperSlide>
+          <div className="flex flex-col">
+            <div className="active-user bg-user-1">
+              <Image src="/person/person-1.png" alt="tes" width={personWidth} height={personHeight} />
+            </div>
+            <span className="flex justify-center active-user-text text-xl">Person 1</span>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex flex-col">
+            <div className="active-user bg-user-1">
+              <Image src="/person/person-1.png" alt="tes" width={personWidth} height={personHeight} />
+            </div>
+            <span className="flex justify-center active-user-text text-xl">Person 1</span>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex flex-col">
+            <div className="active-user bg-user-1">
+              <Image src="/person/person-1.png" alt="tes" width={personWidth} height={personHeight} />
+            </div>
+            <span className="flex justify-center active-user-text text-xl">Person 1</span>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex flex-col">
+            <div className="active-user bg-user-1">
+              <Image src="/person/person-1.png" alt="tes" width={personWidth} height={personHeight} />
+            </div>
+            <span className="flex justify-center active-user-text text-xl">Person 1</span>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex flex-col">
+            <div className="active-user bg-user-1">
+              <Image src="/person/person-1.png" alt="tes" width={personWidth} height={personHeight} />
+            </div>
+            <span className="flex justify-center active-user-text text-xl">Person 1</span>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex flex-col">
+            <div className="active-user bg-user-1">
+              <Image src="/person/person-1.png" alt="tes" width={personWidth} height={personHeight} />
+            </div>
+            <span className="flex justify-center active-user-text text-xl">Person 1</span>
+          </div>
+        </SwiperSlide>
+      </div>
+      </Swiper>
+
+      <div className="active-container my-4">
+        <span className="text-base font-bold text-white inline-flex items-center">
+          Recents <i className=" ml-1 icon-clock"></i>
+        </span>
+      </div>
+
+      <div className="chat-section">
+      {dataChat.map((data) => (
+      <div key={data.id} className="chat-container gap-2">
+          <div className="img-chat-container bg-user-1">
+            <Image src="/person/person-1.png" alt="" width={personChatWidth} height={personChatHeight} />
+          </div>
+          <div className="flex flex-row flex-auto">
+            <div className="flex flex-col flex-auto justify-evenly">
+              <span>{data.name}</span>
+              <span>Hey, whats up</span>
+            </div>
+            <div className="flex flex-col justify-evenly">
+              <span className="text-purple font-semibold">4 min</span>
+              <span className="flex justify-end"> <span className="bg-purple aspect-square unread">{data.unread}</span></span>
+            </div>
+          </div>
+      </div>
+      ))}
+      </div>
+      
+    </div>
+    <div className="root fixed flex bottom-0 left-0 right-0 mx-auto z-2">
+        <div className="menu bg-semi-black flex-auto flex-row flex justify-between px-6 py-4 mx-4 mb-2">
+          <div className="icon-chat"></div>
+          <div className="icon-call"></div>
+          <div className="icon-camera"></div>
+          <div className="icon-setting"></div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
